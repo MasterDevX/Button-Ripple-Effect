0ac17ab957a7291fcbc04e58afb6286e22b3f012
MasterDevX <32103950+MasterDevX@users.noreply.github.com>
2018-12-10 17:16:17 +0200

    Rewriting

--- button.js
+++ button.js
@@ -1,22 +1,14 @@
-var buttons = document.getElementsByTagName('button');
-
-Array.prototype.forEach.call(buttons, function (b) {
-    b.addEventListener('click', createRipple);
+button = document.getElementsByClassName("ripplebutton");
+Array.prototype.forEach.call(button, function (b){
+    b.addEventListener("click", createRipple);
 });
-
-function createRipple (e) {
-    var circle = document.createElement('div');
+function createRipple (e){
+    circle = document.createElement("div");
     this.appendChild(circle);
-
-    var d = Math.max(this.clientWidth, this.clientHeight);
-
-    circle.style.width = circle.style.height = d + 'px';
-
-var rect = this.getBoundingClientRect();
-circle.style.left = e.clientX - rect.left -d/2 + 'px';
-circle.style.top = e.clientY - rect.top - d/2 + 'px';
-
-    console.log(this);
-
-    circle.classList.add('ripple');
+    d = Math.max(this.clientWidth, this.clientHeight);
+    circle.style.width = circle.style.height = d + "px";
+    rect = this.getBoundingClientRect();
+    circle.style.left = e.clientX - rect.left -d/2 + "px";
+    circle.style.top = e.clientY - rect.top - d/2 + "px";
+    circle.classList.add("ripple");
 }
